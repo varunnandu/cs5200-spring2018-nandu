22a869c7edeb0b309fa83bb317d2549273b3085a
Varun Nandu <varunnandu@Rajeshs-MacBook-Pro.local>
2018-01-14 18:54:56 -0500

    first commit

---
+++ src/main/java/edu/northeastern/cs5200/Cs5200Spring2018NanduApplication.java
@@ -0,0 +1,13 @@
+package edu.northeastern.cs5200;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Cs5200Spring2018NanduApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs5200Spring2018NanduApplication.class, args);
+	}
+}
