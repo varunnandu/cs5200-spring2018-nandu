e79497d4bf6e7a83b87d184691c84f88bdbf4e2c
varunnandu1992 <varunnandu1992@ccs.neu.edu>
2018-02-04 11:16:52 -0500

    PR2 completed

--- src/main/java/edu/northeastern/cs5200/Cs5200Spring2018NanduApplication.java
+++ src/main/java/edu/northeastern/cs5200/Cs5200Spring2018NanduApplication.java
@@ -2,10 +2,18 @@ package edu.northeastern.cs5200;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Cs5200Spring2018NanduApplication extends SpringBootServletInitializer{
+	
+	@Override
+	 protected SpringApplicationBuilder
+	  configure(SpringApplicationBuilder application) {
+	  return application.sources(
+			  Cs5200Spring2018NanduApplication.class);
+	 }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cs5200Spring2018NanduApplication.class, args);
--- src/main/java/edu/northeastern/cs5200/controllers/hello/HelloController.java
+++ src/main/java/edu/northeastern/cs5200/controllers/hello/HelloController.java
@@ -1,7 +1,9 @@
 package edu.northeastern.cs5200.controllers.hello;
 
+import java.util.List;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +19,30 @@ public class HelloController {
   HelloObject obj = new HelloObject("Hello Varun Nandu!");
   return obj;
  }
+ 
+ @Autowired
+ HelloRepository helloRepository;
+ 
+ @RequestMapping("/api/hello/insert")
+ public HelloObject insertHelloObject() {
+  HelloObject obj = new HelloObject("Hello Varun Nandu!");
+  helloRepository.save(obj);
+  return obj;
+ }
+ 
+
+@RequestMapping("/api/hello/insert/{msg}")
+public HelloObject insertMessage(@PathVariable("msg") String message) {
+ HelloObject obj = new HelloObject(message);
+ helloRepository.save(obj);
+ return obj;
+}
+
+
+@RequestMapping("/api/hello/select/all")
+public List<HelloObject> selectAllHelloObjects() {
+ List<HelloObject> hellos =
+  (List<HelloObject>)helloRepository.findAll();
+ return hellos;
+}
 }
